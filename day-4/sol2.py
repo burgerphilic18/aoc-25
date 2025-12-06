be1aea290b0d5ed641e93a1a053654b064cac6e9
Spandan Hota <spandanhota2005@outlook.com>
2025-12-06 21:02:27 +0530

    day 4 part 2 done
    Signed-off-by: Spandan Hota <spandanhota2005@outlook.com>

---
+++ day-4/sol2.py
@@ -0,0 +1,34 @@
+ctr = 0
+grid = []
+with open("input.txt", "r") as file:
+    content = file.read()
+    grid = [list(x) for x in content.splitlines()]
+
+rows = len(grid)
+cols = len(grid[0])
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+while True:
+    rem = []
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == '@':
+                adj = 0
+                for k in range(8):
+                    ni = i + dx[k]
+                    nj = j + dy[k]
+                    if 0 <= ni < rows and 0 <= nj < cols:
+                        if grid[ni][nj] == '@':
+                            adj += 1
+                if adj < 4:
+                    rem.append((i, j))
+    
+    if len(rem) == 0:
+        break
+        
+    ctr += len(rem)
+    for r, c in rem:
+        grid[r][c] = '.'
+
+print('answer is', ctr)
